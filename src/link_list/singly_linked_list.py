3987688e9ceffe523e925e0a1243b0914e73956c
thomaszdxsn <bnm_965321@sina.com>
2020-05-09 23:14:12 +0800

    👷 on the working

--- src/link_list/singly_linked_list.py
+++ src/link_list/singly_linked_list.py
@@ -6,7 +6,6 @@
     insert/delete in middle          O(1) + search time
     waste space                      O(n)
 
-
 https://en.wikipedia.org/wiki/Linked_list
 """
 from __future__ import annotations
@@ -14,7 +13,7 @@ from typing import Any, Iterable, Optional
 
 import attr
 
-__all__  = ('SinglyLinkList',)
+__all__ = ('SinglyLinkList',)
 
 
 @attr.s(slots=True, auto_attribs=True)
@@ -23,13 +22,16 @@ class Node:
     prev: Optional[Node] = None
     next: Optional[Node] = None
 
+    def __str__(self):
+        return str(self.value)
+
 
 class SinglyLinkList:
 
     def __init__(self, iterable: Optional[Iterable] = None):
         self.length: int = 0
-        self.head: Optional[Node] = None
-        self.tail: Optional[Node] = None
+        self._head: Optional[Node] = None
+        self._tail: Optional[Node] = None
 
         if iterable:
             [self.append(node) for node in iter(iterable)]
@@ -37,22 +39,51 @@ class SinglyLinkList:
     def __len__(self):
         return self.length
 
+    def __iter__(self):
+        node = self._head
+        while node:
+            yield node.value
+            node = node.next
+
+    @property
+    def tail(self) -> Optional[any]:
+        """return tail's value if exists"""
+        return self._tail and self._tail.value
+
+    @property
+    def head(self) -> Optional[any]:
+        """return head's value if exists"""
+        return self._head and self._head.value
+
+    def _add_head(self, value: Any):
+        assert not self.head, 'can\'t add head when head exists'
+        node = Node(value=value)
+        self._head = node
+        self._tail = node
+        self.length += 1
+
     def append(self, value: Any):
-        pass
+        if not self.head:
+            self._add_head(value)
+            return
+        self.length += 1
+        node = Node(value=value, prev=self._tail)
+        node.prev.next = node
+        self._tail = node
 
     def prepend(self, value: Any):
-        self.length += 1
         if not self.head:
-            node = Node(value=value)
-            self.head = node
-            self.tail = node
+            self._add_head(value)
             return
-
-        node = Node(value=value, next=self.head)
+        self.length += 1
+        node = Node(value=value, next=self._head)
         node.next.prev = node
+        self._head = node
 
     def insert(self, index: int, node: Node):
+        # TODO: must implementation __getitem__ API
         pass
 
-    def extend(self, iterable: Iterable):
-        pass
+    def extend(self, iterable: Iterable[Any]):
+        for item in iterable:
+            self.append(item)
--- src/link_list/tests/test_singly_linked_list.py
+++ src/link_list/tests/test_singly_linked_list.py
@@ -6,10 +6,55 @@ def test_prepend_in_empty_linked_list():
     assert len(linked_list) == 0
 
     linked_list.prepend('test')
-    assert linked_list.head.value == 'test'
-    assert linked_list.tail.value == 'test'
+    assert linked_list.head == 'test'
+    assert linked_list.tail == 'test'
     assert len(linked_list) == 1
 
 
 def test_prepend_in_non_empty_linked_list():
-    pass
+    linked_list = SinglyLinkList([1, 2, 3])
+    assert len(linked_list) == 3
+    assert linked_list.head == 1
+
+    linked_list.prepend(0)
+    assert len(linked_list) == 4
+    assert linked_list.head == 0
+    assert list(linked_list) == [0, 1, 2, 3]
+
+
+def test_append_in_empty_linked_list():
+    linked_list = SinglyLinkList()
+    assert len(linked_list) == 0
+
+    linked_list.append('test')
+    assert linked_list.head == 'test'
+    assert linked_list.tail == 'test'
+    assert len(linked_list) == 1
+
+
+def test_append_in_non_empty_linked_list():
+    linked_list = SinglyLinkList([1, 2, 3])
+    assert len(linked_list) == 3
+    assert linked_list.tail == 3
+
+    linked_list.append(4)
+    assert linked_list.tail == 4
+    assert list(linked_list) == [1, 2, 3, 4]
+
+
+def test_extend_in_empty_linked_list():
+    linked_list = SinglyLinkList()
+    assert len(linked_list) == 0
+
+    linked_list.extend([1, 2, 3])
+    assert len(linked_list) == 3
+    assert list(linked_list) == [1, 2, 3]
+
+
+def test_extend_in_non_empty_linked_list():
+    linked_list = SinglyLinkList([1, 2, 3])
+    assert len(linked_list) == 3
+
+    linked_list.extend([4, 5, 6])
+    assert len(linked_list) == 6
+    assert list(linked_list) == [1, 2, 3, 4, 5, 6]
